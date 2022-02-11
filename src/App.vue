6ec2cc042f7f12a1569f2336720171fd75b92c3d
PMV-cute <victor_black1998@mail.ru>
2022-02-11 10:50:10 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="app">
+    {{firstName}}
+    {{secondName}}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+name: 'App',
+components: {},
+  data(){
+    return{
+      firstName:'Ласкин',
+      secondName: 'Максим'
+      }
+    }
+});
+</script>
+
+<style>
+
+</style>
